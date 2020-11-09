774cdcba87da0e3462da3016931772efe7d0d175
David Ralph <davetheprogrammer2@gmail.com>
2020-11-09 16:19:30 +0000

    use env

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const fastify = require('fastify')();
 const fetch = require('centra');
 const Logger = require('leekslazylogger-fastify');
 const config = require('./config.json');
+require('dotenv').config();
 
 //* Set stuff
 const log = new Logger({
@@ -21,14 +22,14 @@ fastify.get('/', async () => {
 });
 
 fastify.get('/getImage', async (_req, res) => {
-  const data = await (await fetch(`https://api.unsplash.com/photos/random?client_id=${config.unsplashkey}&query=nature&content_filter=high&featured=true&orientation=landscape`).send()).json();
+  const data = await (await fetch(`https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_KEY}&query=nature&content_filter=high&featured=true&orientation=landscape`).send()).json();
   res.send({
     file: data.urls.full + '&w=1920',
     photographer: data.user.name,
     location: data.location.city + ' ' + data.location.country,
     photographer_page: data.user.links.html + '?utm_source=mue&utm_medium=referral'
   });
-  await fetch(`${data.links.download_location}?client_id=${config.unsplashkey}`).send();
+  await fetch(`${data.links.download_location}?client_id=${process.env.UNSPLASH_KEY}`).send();
 });
 
 //* Listen on port
